e72ea7c8c97b09dc89d1da449349e98331443280
Gabriel Bourcier <gmelanson.bourcier@gmail.com>
2026-05-08 15:19:40 -0400

    fixed build + settings + configured debugger

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"errors"
+	"fmt"
 	"os"
 )
 
@@ -11,16 +12,33 @@ type Config struct {
 }
 
 func Load() (*Config, error) {
-	dbURL := os.Getenv("DATABASE_URL")
-	if dbURL == "" {
-		return nil, errors.New("DATABASE_URL is required")
+	user := os.Getenv("POSTGRES_USER")
+	if user == "" {
+		return nil, errors.New("POSTGRES_USER is required")
+	}
+	password := os.Getenv("POSTGRES_PASSWORD")
+	if password == "" {
+		return nil, errors.New("POSTGRES_PASSWORD is required")
+	}
+	db := os.Getenv("POSTGRES_DB")
+	if db == "" {
+		return nil, errors.New("POSTGRES_DB is required")
+	}
+	host := os.Getenv("POSTGRES_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	port := os.Getenv("POSTGRES_PORT")
+	if port == "" {
+		port = "5432"
 	}
 	addr := os.Getenv("HTTP_ADDR")
 	if addr == "" {
 		addr = "127.0.0.1:3000"
 	}
+
 	return &Config{
-		DatabaseURL: dbURL,
+		DatabaseURL: fmt.Sprintf("postgres://%s:%s@%s:%s/%s", user, password, host, port, db),
 		HTTPAddr:    addr,
 	}, nil
 }
