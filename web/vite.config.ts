45bd93d0ad26d8f85cbc26d29931b3d0e49cfa92
Gabriel Bourcier <gmelanson.bourcier@gmail.com>
2026-05-14 23:02:01 -0400

    styling

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,37 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import ViteFonts from 'unplugin-fonts/vite'
 
 export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true })],
+  plugins: [
+    vue({ template: { transformAssetUrls } }),
+    vuetify({
+      autoImport: true,
+      styles: { configFile: 'src/styles/settings.scss' },
+    }),
+    ViteFonts({
+      google: {
+        families: [
+          { name: 'Roboto', styles: 'wght@100;300;400;500;700;900' },
+        ],
+      },
+    }),
+  ],
+  define: { 'process.env': {} },
+  css: {
+    preprocessorOptions: {
+      scss: { api: 'modern-compiler' },
+      sass: { api: 'modern-compiler' },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
+  },
   server: {
     port: 5173,
     proxy: {
